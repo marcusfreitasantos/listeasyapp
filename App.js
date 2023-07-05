dcda2c8050dede38685e1759c42a1713460bbf7b
Marcus Freitas <marcusfreitasantos@gmail.com>
2023-07-05 18:41:41 -0300

    sentry

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import { useEffect } from "react";
-import { useColorScheme } from "react-native";
+import { useColorScheme, ActivityIndicator, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import MainStack from "./src/stacks/MainStack";
 import { createListsTable } from "./src/services/ListQueries";
 import { createItemsTable } from "./src/services/ItemQueries";
 import { InfoProvider } from "./src/contexts/GlobalContext";
-import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/theme";
 import darkTheme from "./src/global/darkTheme";
-import { ActivityIndicator } from "react-native";
 import "expo-dev-client";
-import { REVENUECAT_KEY_ANDROID } from "@env";
+import { REVENUECAT_KEY_ANDROID, SENTRY_DSN } from "@env";
 import Purchases from "react-native-purchases";
+import * as Sentry from "sentry-expo";
 
 import {
   useFonts,
@@ -27,6 +26,12 @@ export default function App() {
   });
 
   const colorScheme = useColorScheme();
+
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    enableInExpoDevelopment: true,
+    debug: true,
+  });
 
   useEffect(() => {
     createListsTable();
