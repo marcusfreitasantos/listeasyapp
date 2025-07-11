218b25bb1f5f807b1ffcb465b1ace895458c5679
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-07-11 14:55:31 -0300

    [FEAT] List card menu

--- src/features/listsManager/components/listCard/index.tsx
+++ src/features/listsManager/components/listCard/index.tsx
@@ -2,6 +2,9 @@ import * as S from "./styles";
 import { ListEntityType } from "../../model/list";
 import Feather from "@expo/vector-icons/Feather";
 import { useTheme } from "styled-components/native";
+import { ListMenu } from "../listMenu";
+import { useState } from "react";
+import { FeatherIconName } from "@/@types/icons";
 
 type ListCardProps = {
   list: ListEntityType;
@@ -10,33 +13,54 @@ type ListCardProps = {
 export const ListCard = ({ list }: ListCardProps) => {
   const theme = useTheme();
   const iconSize = Number(theme.defaultSizes.medium.replace("px", ""));
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const openList = () => {
-    console.log("List: ", list.title);
-  };
+  const listMenuOptions = [
+    {
+      label: "Editar",
+      iconName: "edit" as FeatherIconName,
+      onPress: () => console.log("editar"),
+    },
+    {
+      label: "Compartilhar",
+      iconName: "share-2" as FeatherIconName,
+      onPress: () => console.log("comparitlhar"),
+    },
+    {
+      label: "Exportar PDF",
+      iconName: "file-text" as FeatherIconName,
 
-  const openListMenu = () => {
-    console.log("Menu for List: ", list.title);
-  };
+      onPress: () => console.log("Exportar em pdf"),
+    },
+    {
+      label: "Excluir",
+      iconName: "trash" as FeatherIconName,
+      onPress: () => console.log("excluir"),
+    },
+  ];
 
   return (
-    <S.ListCardWrapper onPress={openList}>
-      <S.ListCardInfoWrapper>
-        <S.ListCardTitle numberOfLines={1}>{list.title}</S.ListCardTitle>
-        <S.ListCardTotalPriceWrapper>
-          <S.ListCardTotalPriceTextBold>Total: </S.ListCardTotalPriceTextBold>
+    <S.ListCardWrapper>
+      {isMenuOpen ? (
+        <ListMenu options={listMenuOptions} />
+      ) : (
+        <S.ListCardInfoWrapper>
+          <S.ListCardTitle numberOfLines={1}>{list.title}</S.ListCardTitle>
+          <S.ListCardTotalPriceWrapper>
+            <S.ListCardTotalPriceTextBold>Total: </S.ListCardTotalPriceTextBold>
 
-          <S.ListCardTotalPriceTextRegular>
-            {list.totalPrice}
-          </S.ListCardTotalPriceTextRegular>
-        </S.ListCardTotalPriceWrapper>
-      </S.ListCardInfoWrapper>
+            <S.ListCardTotalPriceTextRegular>
+              {list.totalPrice}
+            </S.ListCardTotalPriceTextRegular>
+          </S.ListCardTotalPriceWrapper>
+        </S.ListCardInfoWrapper>
+      )}
 
-      <S.ListCardMenuBtn onPress={openListMenu}>
+      <S.ListCardMenuBtn onPress={() => setIsMenuOpen(!isMenuOpen)}>
         <Feather
           size={iconSize}
           color={theme.primaryColor}
-          name="more-vertical"
+          name={isMenuOpen ? "x-circle" : "more-vertical"}
         />
       </S.ListCardMenuBtn>
     </S.ListCardWrapper>
---
+++ src/features/listsManager/components/listMenu/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+
+export const ListMenuWrapper = styled.View`
+  flex: 1;
+  gap: ${({ theme }) => theme.defaultSizes.medium};
+`;
+
+export const ListMenuItem = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  gap: ${({ theme }) => theme.defaultSizes.xxSmall};
+`;
+
+export const ListMenuItemText = styled.Text`
+  color: white;
+  font-family: ${({ theme }) => theme.defaultFontFamily};
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.defaultSizes.small};
+`;
