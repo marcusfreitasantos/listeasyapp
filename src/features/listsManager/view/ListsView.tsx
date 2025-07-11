b4ad07f9653e77b260658d7ea23ce185b30cbe4b
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-07-11 15:08:27 -0300

    [FEAT] search list by name

--- src/features/listsManager/view/ListsView.tsx
+++ src/features/listsManager/view/ListsView.tsx
@@ -1,34 +1,36 @@
 import { useState } from "react";
 import { ActivityIndicator } from "react-native";
 import { useTheme } from "styled-components/native";
-import { SampleLists } from "@/src/mocks/lists";
 import { ListCard } from "../components/listCard";
-import { ListEntityType } from "../model/list";
 import { FlatList } from "react-native-gesture-handler";
 import * as S from "./styles";
 import { InputField } from "@/src/components/inputField";
 import { AddItemBtn } from "@/src/components/addItemBtn";
 import { ModalAddList } from "../components/modalAddList";
+import { useListManagerViewModel } from "../viewModel/useListManagerViewModel";
 
 const ListsView = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
   const theme = useTheme();
-  const [currentUserLists, setCurrentUserLists] = useState<
-    ListEntityType[] | []
-  >(SampleLists);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const { loading, searchTerm, setSearchTerm, currentUserLists } =
+    useListManagerViewModel();
 
   return (
     <S.ListView>
-      {isLoading ? (
+      {loading ? (
         <ActivityIndicator color={theme.primaryColor} />
       ) : (
         <>
-          <InputField placeholder="Pesquisar" iconName="search" />
+          <InputField
+            placeholder="Pesquisar"
+            iconName="search"
+            onChangeText={(t) => setSearchTerm(t)}
+          />
 
           <FlatList
-            data={currentUserLists}
+            data={currentUserLists.filter((list) =>
+              list.title.toLowerCase().includes(searchTerm.toLowerCase())
+            )}
             keyExtractor={(item) => item.id.toString()}
             renderItem={({ item }) => <ListCard list={item} />}
           />
--- src/features/listsManager/viewModel/useListManagerViewModel.tsx
+++ src/features/listsManager/viewModel/useListManagerViewModel.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { SampleLists } from "@/src/mocks/lists";
+import { ListEntityType } from "../model/list";
 
-export const useListManagerViewModel = async () => {
+export const useListManagerViewModel = () => {
   const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentUserLists, setCurrentUserLists] = useState<
+    ListEntityType[] | []
+  >(SampleLists);
 
   return {
     loading,
+    searchTerm,
+    setSearchTerm,
+    currentUserLists,
   };
 };
