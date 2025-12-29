cf98b7406c97aa522fa92f14a083610f8db220a1
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-12-29 08:30:41 -0300

    [FEAT] sendSupportEmail util translated

--- src/utils/sendSupportEmail.ts
+++ src/utils/sendSupportEmail.ts
@@ -1,20 +1,19 @@
 import { Linking, Alert } from "react-native";
+import { useTranslation } from "react-i18next";
 
 export const sendSupportEmail = async () => {
+  const { t } = useTranslation();
   const recipient = "contato@mafreitas.com.br";
-  const subject = "[List Easy] Preciso de ajuda!";
+  const subject = t("support_email_subject");
   const url = `mailto:${recipient}?subject=${subject}`;
   try {
     const isSuported = await Linking.canOpenURL(url);
     if (isSuported) {
       await Linking.openURL(url);
     } else {
-      Alert.alert("Oops!", "Não foi possível abrir o e-mail.");
+      Alert.alert(t("Error"), t("email_not_opened"));
     }
   } catch (error) {
-    Alert.alert(
-      "Oops!",
-      `Algo errado aconteceu. Tente novamente mais tarde. Erro: ${error}`
-    );
+    Alert.alert(t("something_wrong"), `${error}`);
   }
 };
