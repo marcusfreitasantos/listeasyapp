37996e3c626a3f0b14d3ab59d3753b4c0ed5c4c5
Marcus Freitas <marcusfreitasantos@gmail.com>
2026-06-20 14:46:00 -0300

    chore: ios build

--- src/context/productsContext.tsx
+++ src/context/productsContext.tsx
@@ -4,18 +4,6 @@ import {
   ProductEntity,
 } from "../features/subscriptions/model/product";
 
-const productsFallback: ProductEntity[] = [
-  {
-    amount: 799,
-    currency: "BRL",
-    description:
-      "Ideal for those who want more focus on their lists, without interruptions.",
-    interval: "month",
-    name: "Essential",
-    productId: "plan_essencial",
-  },
-];
-
 export const GlobalProductsContext = createContext<ProductsContextType>({
   currentProducts: null,
   setCurrentProducts: () => null,
@@ -27,7 +15,7 @@ export const GlobalProductsContext = createContext<ProductsContextType>({
 const ProductsContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [currentProducts, setCurrentProducts] = useState<
     ProductEntity[] | null
-  >(productsFallback);
+  >(null);
 
   const [currency, setCurrency] = useState<string | null>("BRL");
 
